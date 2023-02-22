089a636e48fb6be02df1b2139cec6550b922a520
bohfed <bohdanfedenko@gmail.com>
2023-02-22 17:04:03 +0200

    Update test of todo.cy.js spec

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -12,12 +12,19 @@
 // https://on.cypress.io/introduction-to-cypress
 
 describe('example to-do app', {browser : 'chrome'}, () => {
+
+  let text;
+
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
     cy.visit('https://example.cypress.io/todo')
+
+    cy.xpath('//ul[@class="todo-list"]/li').last().invoke('text').then(txt => {
+      text = txt
+    })
   })
 
   it.only('displays two todo items by default',{browser : 'chrome'}, () => {
@@ -34,12 +41,11 @@ describe('example to-do app', {browser : 'chrome'}, () => {
     // to get just the first and last matched elements individually,
     // and then perform an assertion with `should`.
     cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+    cy.log('Hello, it is - ' + text.toUpperCase())
     cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
-
-    cy.pause()
   })
 
-  it.only('can add new todo items', {browser : '!chrome'}, () => {
+  it.only('can add new todo items', {browser : 'chrome'}, () => {
     // We'll store our item text in a variable so we can reuse it
     const newItem = 'Feed the cat'
 
